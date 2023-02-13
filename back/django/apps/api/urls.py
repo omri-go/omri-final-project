77b3d8b8a741e89d0189e5f2a1987d3cb0bcceb8
c1J2OSAtN9 <87268288+c1J2OSAtN9@users.noreply.github.com>
2023-02-13 15:19:38 +0200

    added example

--- back/django/apps/api/urls.py
+++ back/django/apps/api/urls.py
@@ -1,16 +1,21 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from apps.api import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from apps.api.views import store_views
 
-urlpatterns = [
+
+urlpatterns = [    
+    path('api/message/', store_views.get_message, name='get_message_test'),    
    
     re_path(r'^api/profiles/$', views.profile_list),
     re_path(r'^api/profiles\/(?P<pk>.+)', views.profile_detail),
 
     re_path(r'^api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    re_path(r'^api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   
+
+    
 ]
    
--- front/my-app/src/App.tsx
+++ front/my-app/src/App.tsx
@@ -1,24 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Outlet, Link } from "react-router-dom";
 import { selectCartItems } from "./features/cart/cartSlice";
 import { useAppSelector, useAppDispatch } from "./app/hooks";
 import { selectLogged, logoutAsync } from "./features/Login/loginSlice";
+
+///////////// GET example
+
+import { createStore } from "redux";
+
+const initialState = {
+  message: "",
+};
+
+function reducer(state = initialState, action: { type: any; payload: any; }) {
+  switch (action.type) {
+    case "UPDATE_MESSAGE":
+      return {
+        ...state,
+        message: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+function App2() {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/message/")
+      .then((response) => response.json())
+      .then((data) => {
+        store.dispatch({ type: "UPDATE_MESSAGE", payload: data.message });
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{state.message}</h1>
+    </div>
+  );
+}
+
+//export default App2;
+
+
+///////////// end hello world
+
 function App() {
   const dispatch = useAppDispatch();
   const logged = useAppSelector(selectLogged || false);
   const cartItems = useAppSelector(selectCartItems || []);
+
+
+  //// start GET example
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/message/")
+      .then((response) => response.json())
+      .then((data) => {
+        store.dispatch({ type: "UPDATE_MESSAGE", payload: data.message });
+      });
+  }, []);
+  //// end GET example
+
   return (
     <div className="App">
       <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
-        <Link to="/Products">Products</Link> | <Link to="/about">About</Link> |{" "}
-        <Link to="/categories">Categories</Link>|
+        <Link to="/Products">Products</Link> | 
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/categories">Categories</Link> |  
         {logged ? (
           <button onClick={() => dispatch(logoutAsync())}>Logout</button>
         ) : (
           <Link to="/login">login</Link>
         )}
-        <Link to="/students">students</Link>|
+         | <Link to="/students">students</Link>|
         |{" "}
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -32,8 +105,15 @@ function App() {
         </svg>
         ({cartItems.length})
       </nav>
+
+      {/* this is part of the GET example */
+        <h1>{state.message}</h1>
+      }
+
       <Outlet></Outlet>
     </div>
+
+
   );
 }
 
